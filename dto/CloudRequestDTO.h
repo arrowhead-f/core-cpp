f3eef472ded0281c177707b261dc00cc55b582be
mm10918 <mm10918@ic.ac.uk>
2020-08-07 16:49:42 +0200

    Created CloudRequestDTO.h and implemented getter, setter functions

---
+++ dto/CloudRequestDTO.h
@@ -0,0 +1,108 @@
+#ifndef CORE_CPP_CLOUDREQUESTDTO_H
+#define CORE_CPP_CLOUDREQUESTDTO_H
+
+/// STD libs
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <vector>
+
+
+using llint = long long int;
+using MyInt = int;
+
+/// DTO class storing cloud requests
+class CloudRequestDTO {
+
+    private:
+        static const long long int serialVersionUID = 7397917411719621910L;
+
+        ///Private members
+        std::string cloudOperator;
+        std::string name;
+        bool secure;
+        bool neighbor;
+        std::string authenticationInfo;
+        std::vector<llint> gatekeeperRelayIds;
+        std::vector<llint> gatewayRelayIds;
+
+    public:
+    ///Getter functions
+        /*!
+        * Getter function returning cloudOperator
+        * @return cloudOperator
+        */
+        std::string getCloudOperator() const { return cloudOperator; }
+        /*!
+        * Getter function returning name
+        * @return name
+        */
+        std::string getName() const { return name; }
+        /*!
+        * Getter function returning secure
+        * @return secure
+        */
+        bool getSecure() const { return secure; }
+        /*!
+        * Getter function returning neighbor
+        * @return neighbor
+        */
+        bool getNeighbor() const { return neighbor; }
+        /*!
+        * Getter function returning authenticationInfo
+        * @return authenticationInfo
+        */
+        std::string getAuthenticationInfo() const { return authenticationInfo; }
+        /*!
+        * Getter function returning gatekeeperRelayIds
+        * @return gatekeeperRelayIds
+        */
+        std::vector<llint> getGatekeeperRelayIds() const { return gatekeeperRelayIds; }
+        /*!
+        * Getter function returning gatewayRelayIds
+        * @return gatewayRelayIds
+        */
+        std::vector<llint> getGatewayRelayIds() const { return gatewayRelayIds; }
+
+
+    /// Setter functions
+        /*!
+        * Setter function setting cloudOperator
+        * @param[in] cloudOperator
+        */
+        void setCloudOperator(const std::string cloudOperator) { this->cloudOperator = cloudOperator; }
+        /*!
+        * Setter function setting name
+        * @param[in] name
+        */
+        void setName(const std::string name) { this->name = name; }
+        /*!
+        * Setter function setting secure
+        * @param[in] secure
+        */
+        void setSecure(const bool secure) { this->secure = secure; }
+        /*!
+        * Setter function setting neighbor
+        * @param[in] neighbor
+        */
+        void setNeighbor(const bool neighbor) { this->neighbor = neighbor; }
+        /*!
+        * Setter function setting authenticationInfo
+        * @param[in] authenticationInfo
+        */
+        void setAuthenticationInfo(const std::string authenticationInfo) { this->authenticationInfo = authenticationInfo; }
+        /*!
+        * Setter function setting gatekeeperRelayIds
+        * @param[in] gatekeeperRelayIds
+        */
+        void setGatekeeperRelayIds(const std::vector<llint>& gatekeeperRelayIds) { this->gatekeeperRelayIds = gatekeeperRelayIds; }
+        /*!
+        * Setter function setting gatewayRelayIds
+        * @param[in] gatewayRelayIds
+        */
+        void setGatewayRelayIds(const std::vector<llint> gatewayRelayIds) { this->gatewayRelayIds = gatewayRelayIds; }
+
+};
+
+
+#endif //CORE_CPP_CLOUDREQUESTDTO_H
