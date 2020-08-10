df2651e4eb4a10c55c661f8e739c463892d0a836
mm10918 <mm10918@ic.ac.uk>
2020-08-10 18:12:58 +0200

    Implemented generateJwtId()

--- TokenGenerationService.h
+++ TokenGenerationService.h
@@ -1,11 +1,26 @@
 #ifndef CORE_CPP_TOKENGENERATIONSERVICE_H
 #define CORE_CPP_TOKENGENERATIONSERVICE_H
 
+///Header includes
+#include "jwt/jwt.hpp"
+#include "dto/SystemRequestDTO.h"
+#include "dto/TokenGenerationProviderDTO.h"
+#include "dto/TokenGenerationRequestDTO.h"
+#include "dto/CloudRequestDTO.h"
+#include "Utils/base64.h"
+
+
 ///STD Libs
 #include <iostream>
 #include <vector>
 #include <string>
 #include <map>
+#include <chrono>
+#include <algorithm>
+#include <functional>
+#include <random>
+#include <bitset>
+#include <array>
 
 class TokenGenerationService {
 
@@ -26,7 +41,25 @@ class TokenGenerationService {
         //TODO: Implement ServiceInterfaceNameVerifier
 
         std::string ownCloudName;
-        std::string ownCloudOperator;
+        std::string ownCloudOperator
+
+    public:
+
+        ///Randomly generates JwtId
+        std::string generateJwtId(){
+            std::random_device rd;
+            auto seed_data = std::array<int, std::mt19937::state_size> {};
+            std::generate(std::begin(seed_data), std::end(seed_data), std::ref(rd));
+            std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+            std::mt19937 generator(seq);
+            std::uniform_int_distribution<short> dis(std::numeric_limits<short>::min(),
+                                                     std::numeric_limits<short>::max());
+
+            std::string n = (std::bitset<16>(dis(generator)).to_string());
+
+            return Base64::Encode(n);
+        };
+
 };
 
 
