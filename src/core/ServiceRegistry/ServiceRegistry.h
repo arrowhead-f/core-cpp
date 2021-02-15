e714620b624e7fb9409eb764fc7502ae04d404ea
Tamas <tothfalusi@aiti.ai>
2021-02-15 14:37:48 +0100

    ServiceRegistry module /echo url handling

--- src/core/ServiceRegistry/ServiceRegistry.h
+++ src/core/ServiceRegistry/ServiceRegistry.h
@@ -11,13 +11,17 @@ template<typename DBPool, typename RB>class ServiceRegistry final : public Core<
 
         using Parent = Core<DBPool, RB>;
 
+    private:
+
 
     public:
 
         using Core<DBPool, RB>::Core;
 
         Response handleGET(Request &&req) final {
-            return Response::from_stock(http::status_code::NotImplemented);
+            if (!req.uri.compare("/echo")){ return Response{"Got it!"}; }
+
+            return Response::from_stock(http::status_code::NotFound);
         }
 
         Response handlePOST(Request &&req) final {
